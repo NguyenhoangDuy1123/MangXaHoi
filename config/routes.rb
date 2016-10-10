b4728ec91bab9ab7405035a9fdba1048333c6716
NguyenhoangDuy1123 <nghduy11011995@gmail.com>
2016-10-11 03:04:13 +0700

    Fix lỗi

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+	resources :users do
+		member do
+			get :following, :followers
+		end
+	end
 
-  resources :users
-  
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
-  resources :microposts,	only: [:create, :destroy]
-  root 'sessions#root'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	get    '/login',   to: 'sessions#new'
+	post   '/login',   to: 'sessions#create'
+	delete '/logout',  to: 'sessions#destroy'
+	
+	resources :microposts,	only: [:create, :destroy]
+	resources :relationships,	only: [:create, :destroy]
+	
+	root 'sessions#root'
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
